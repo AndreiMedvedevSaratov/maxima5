b91c0730648f903afc146e62103a0228de39cb21
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2023-12-14 22:33:41 +0400

    консультация от 14.12.23

---
+++ js/index3.js
@@ -0,0 +1,18 @@
+let person = {
+  name: "Vasya",
+  surname: "Petrov",
+  age: 25,
+  address: {
+    zipCode: 410000,
+    city: "Saratov",
+    street: "Moskovskaya",
+    house: 1,
+    flat: 25,
+  },
+};
+
+console.log(person);
+console.log(person.address.house);
+console.log(person.isAdmin);
+
+console.log(person["address"]);
